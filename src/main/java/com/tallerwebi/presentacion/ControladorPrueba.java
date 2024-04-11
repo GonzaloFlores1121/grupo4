91c339236aa8be06ee6cc05a3934f89be30aba8a
GonzaloFlores1121 <gonzalomarcos551@gmail.com>
2024-04-11 11:08:49 -0300

    primera prueba

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorPrueba.java
@@ -0,0 +1,23 @@
+package com.tallerwebi.presentacion;
+
+import com.tallerwebi.dominio.Usuario;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorPrueba {
+
+    @RequestMapping("/irsaludo")
+    public ModelAndView saludar(){
+
+        ModelMap modelo = new ModelMap();
+        Usuario user= new Usuario();
+        modelo.put("dia","Jueves");
+        modelo.put("mes","Abril");
+        modelo.put("user",user);
+    user.setEmail("panchovilla@gmail.com");
+        return new ModelAndView("saludo",modelo);
+    }
+}
