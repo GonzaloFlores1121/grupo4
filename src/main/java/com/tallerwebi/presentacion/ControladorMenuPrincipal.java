81c1d5bad2afb5d33e2e81ecf465935f86272cd3
MilagrosChavezz <miluchavez2005@gmail.com>
2024-04-30 20:05:31 -0300

    vistas nuevas
    diario de ejercicio , ejercicio y en forma

---
+++ src/main/java/com/tallerwebi/presentacion/ControladorMenuPrincipal.java
@@ -0,0 +1,33 @@
+package com.tallerwebi.presentacion;
+
+
+import com.tallerwebi.dominio.Usuario;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ControladorMenuPrincipal {
+
+    @RequestMapping(value = "/actividadesFisicas",method = RequestMethod.GET)
+    public ModelAndView irAEnForma(){
+
+        return new ModelAndView("actividadesFisicas");
+    }
+
+    @RequestMapping(value = "/ejercicio",method = RequestMethod.GET)
+    public ModelAndView irAEjercicio(){
+
+        return new ModelAndView("ejercicio");
+    }
+
+    @RequestMapping(value = "/diarioEjercicio",method = RequestMethod.GET)
+    public ModelAndView irADiarioEjercicio(){
+
+        return new ModelAndView("diarioEjercicio");
+    }
+
+}
