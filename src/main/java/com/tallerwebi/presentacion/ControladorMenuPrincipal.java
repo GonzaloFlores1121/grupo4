7fbb11fa4175447ceb9caf76624c4911f9e97be9
Rodolfo Gutierrez <rgutierrez649@alumno.unlam.edu.ar>
2024-05-01 20:08:53 -0300

    perfil-notificaciones

--- src/main/java/com/tallerwebi/presentacion/ControladorMenuPrincipal.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorMenuPrincipal.java
@@ -2,6 +2,10 @@ package com.tallerwebi.presentacion;
 
 
 import com.tallerwebi.dominio.Usuario;
+
+import java.util.TreeMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +32,36 @@ public class ControladorMenuPrincipal {
     public ModelAndView irADiarioEjercicio(){
 
         return new ModelAndView("diarioEjercicio");
+    }
+
+    //probando un poco lo del modelo 
+    @RequestMapping(path="/perfilUsuario", method=RequestMethod.GET)
+    public ModelAndView perfilUsuario() {
+        ModelMap modelo = new ModelMap();
+        modelo.put("usuario", "admin");
+        modelo.put("contraseña", "1234");
+        modelo.put("email", "admin1234@gmail.com");
+        modelo.put("edad", 25);
+        modelo.put("genero", "Masculino");
+        modelo.put("altura", 155);
+        modelo.put("peso", 45);
+        modelo.put("nivelDeActividadFisica", "Sedentaria");
+        modelo.put("dietaObjetivo", "Mantener mi peso actual");
+        modelo.put("pesoMeta", 45);
+        return new ModelAndView("perfilUsuario", modelo);
+    }
+
+    //viendo que tal va con un map a posteriori estas weas se borraran, pero solo para ir viendo :v
+    @RequestMapping(path="/notificaciones", method=RequestMethod.GET) 
+    public ModelAndView notificaciones() {
+        ModelMap modelo = new ModelMap();
+        Map<String, String> notificaciones = new TreeMap<>();
+        notificaciones.put("Control Peso", "A las 8 a.m. debes controlar tu peso.");
+        notificaciones.put("Alimentacion", "No olvides que a las 12 a.m. debe almorzar de manera saludable.");
+        notificaciones.put("Mantenimiento", "Desde las 8 p.m. a las 10 p.m. la aplicacion se hallara en mantenimiento gracias por su comprension.");
+        notificaciones.put("Novedades", "No olvides leer el parche con las nuevas actualizaciones.");
+        modelo.addAttribute("notificaciones", notificaciones);
+        return new ModelAndView("notificaciones", modelo);
     }
 
 }
