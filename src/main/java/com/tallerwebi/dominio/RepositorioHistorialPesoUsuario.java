b770258807fe8fd38c216ac3109e93e4e8edc4d6
MilagrosChavezz <143010028+MilagrosChavezz@users.noreply.github.com>
2024-06-01 16:12:16 -0300

    test Modificar Peso

--- src/main/java/com/tallerwebi/dominio/RepositorioHistorialPesoUsuario.java
+++ src/main/java/com/tallerwebi/dominio/RepositorioHistorialPesoUsuario.java
@@ -6,6 +6,7 @@ import java.util.List;
 public interface RepositorioHistorialPesoUsuario {
 
     void agregarPesoYFecha(HistoriaPesoUsuario nuevoPesoUsuario);
+
     List<HistoriaPesoUsuario> obtenerHistorialPesoUsuario();
 
     HistoriaPesoUsuario obtenerHistorialPesoUsuarioParaUnaFecha(Date fecha);
